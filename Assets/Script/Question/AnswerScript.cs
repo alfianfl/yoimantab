c95a13d95673af4c598b11e96dd991ec08658046
Alfarisg 34 <alfarisg34@gmail.com>
2021-05-04 22:46:49 +0700

    Change Scene

---
+++ Assets/Script/Question/AnswerScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class AnswerScript : MonoBehaviour
+{
+    public bool isCorrect = false;
+    public QuizManager quizManager;
+    public SceneSwitch scene;
+    public void Answer()
+    {
+        if(isCorrect)
+        {
+            Debug.Log("Correct Answer");
+            SceneManager.LoadScene(0);
+            quizManager.correct();
+        }
+        else
+        {
+            Debug.Log("Wrong Answer");
+                        SceneManager.LoadScene(0);
+            quizManager.correct();
+        }
+    }
+}
---
+++ Assets/Script/Question/SceneSwitch.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneSwitch : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider other) {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+        SceneManager.LoadScene(1);
+
+    }
+}
